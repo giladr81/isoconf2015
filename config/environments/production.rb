c608ec56597e7dc6305c8c8172a21cefc8708eb6
giladr81 <giladrainer@gmail.com>
2014-03-04 19:16:53 +0200

    fixing devise

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -78,5 +78,5 @@ IsoconfApp::Application.configure do
   # Use default logging formatter so that PID and timestamp are not suppressed.
   config.log_formatter = ::Logger::Formatter.new
 
-  # config.action_mailer.default_url_options = { :host => 'immense-fjord-7393.herokuapp.com' }
+  config.action_mailer.default_url_options = { :host => 'immense-fjord-7393.herokuapp.com' }
 end
