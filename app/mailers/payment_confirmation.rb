d49e477724babed804467769b7d28ee9d9d1ad94
giladr81 <giladrainer@gmail.com>
2014-11-22 15:49:05 +0200

    confirmation email

--- app/mailers/payment_confirmation.rb
+++ app/mailers/payment_confirmation.rb
@@ -2,12 +2,16 @@ class PaymentConfirmation < ActionMailer::Base
  
   def confirmation_email(participant)
   	@payed_user = participant
-  	mail(to: @payed_user.email, from: "isotopes2015@conferences-travel-nevet.com", subject: 'Isotopes 2015 registration confirmation')
+  	mail(to: @payed_user.email,
+  		 from: "isotopes2015@conferences-travel-nevet.com",
+  		 subject: 'Isotopes 2015 registration confirmation')
   end
 
   def confirmation_unsucessful(participant)
   	@payed_user = participant
-  	mail(to: "giladrainer@gmail.com", from: "isotopes2015@conferences-travel-nevet.com", subject: 'Isotopes 2015 - Payment failure')
+  	mail(to: "giladrainer@gmail.com",
+  		 from: "isotopes2015@conferences-travel-nevet.com",
+  		 subject: 'Isotopes 2015 - Payment failure')
   end
 
 end
