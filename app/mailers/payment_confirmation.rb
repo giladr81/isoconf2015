d387bf9c4fbc89e0117665c972c5150787b8f072
giladr81 <giladrainer@gmail.com>
2014-11-22 15:39:39 +0200

    confirmation email

--- app/mailers/payment_confirmation.rb
+++ app/mailers/payment_confirmation.rb
@@ -1,6 +1,5 @@
 class PaymentConfirmation < ActionMailer::Base
-  default from: "isotopes2015@gmail.com"
-
+ 
   def confirmation_email(participant)
   	@payed_user = participant
   	mail(to: @payed_user.email, subject: 'Isotopes 2015 registration confirmation')
